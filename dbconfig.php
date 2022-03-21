b6f111b6e943019e594e07d097ed08aea5a5304f
Md Atikul Haider <atikul4u@gmail.com>
2022-03-21 15:25:00 +0600

    Create dbconfig.php

---
+++ dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+//making the connection with pos_system
+
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$dbname = "leave_management";
+// db connection
+$connect = new mysqli($localhost, $username, $password, $dbname);
+?>
